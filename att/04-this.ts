0dd9f699931a1218d67c3b6c0c9af08438b8aea4
kielop D. rivia <oliveira.marcos16@escola.pr.gov.br>
2026-09-17 20:09:52 -0300

    Update 04-this.ts

--- att/04-this.ts
+++ att/04-this.ts
@@ -1,32 +1,17 @@
-/**
- * Questão 4: this/self
- * Enunciado: Crie uma classe Retangulo com os atributos largura e altura. 
- * Utilize this para inicializá-los no construtor e implementar um método calcularPerimetro().
- */
-
-class Retangulo {
+class retangulo {
     largura: number;
     altura: number;
 
-    constructor(largura: number, altura: number) {
-        // O 'this' diferencia o atributo da classe do parâmetro recebido no construtor
+    constructor(largura: number, altura: number){
         this.largura = largura;
         this.altura = altura;
     }
 
-    // Método que utiliza os atributos internos através do 'this' para calcular o perímetro
-    calcularPerimetro(): number {
-        return 2 * (this.largura + this.altura);
+    CalcularPerimetro(): number {
+        return 2* (this.largura + this.altura)
     }
 }
 
-const meuRetangulo = new Retangulo(10, 5);
+const retangulo1 = new retangulo(10, 5);
 
-console.log("=========================================");
-console.log("          CÁLCULO DO RETÂNGULO           ");
-console.log("=========================================");
-console.log(`Largura: ${meuRetangulo.largura}m`);
-console.log(`Altura: ${meuRetangulo.altura}m`);
-console.log(`Perímetro Total: ${meuRetangulo.calcularPerimetro()}m`);
-console.log("=========================================");
-
+console.log("perimetro:", retangulo1.CalcularPerimetro())
